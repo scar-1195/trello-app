7b779d87e44bea0278b8e3e8f9bd42b3cac37bbb
oscar <o.lrodriguez1195@gmail.com>
2024-02-08 22:16:50 -0600

    proyect_init

---
+++ src/TrelloApp.tsx
@@ -0,0 +1,8 @@
+export const TrelloApp: React.FC = () => {
+  return (
+    <>
+      <h1>TrelloApp</h1>
+      <hr />
+    </>
+  );
+};
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { TrelloApp } from './TrelloApp';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <TrelloApp />
+  </React.StrictMode>,
+);
